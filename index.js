f09ba7e0b921cf494f5572c803d52d1853cb844b
Fi Mellink <fimellink@gmail.com>
2023-12-07 07:57:40 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Fi";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toLocaleUpperCase() + "!";
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = "Welcome, " + firstInitial + "!";
